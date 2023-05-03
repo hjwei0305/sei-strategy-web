a7d73c24b4897781e8d06ee88d92f25d4496929d
SEI管理员 <jun3.xiong@changhong.com>
2023-05-03 07:41:13 +0000

    初始化项目文件

---
+++ config/proxy.config.js
@@ -0,0 +1,14 @@
+export default {
+  '/mocker.api': {
+    target: 'http://rddgit.changhong.com:7300/mock/5dd5efbdc239b926aeb04627/seid.api',
+    changeOrigin: true,
+    secure: false,
+    pathRewrite: { '^/mocker.api': '' },
+  },
+  '/api-gateway': {
+    target: 'http://10.233.0.170/api-gateway',
+    changeOrigin: true,
+    secure: false,
+    pathRewrite: { '^/api-gateway': '' },
+  },
+};
---
+++ config/router.config.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    path: '/user',
+    component: '../layouts/LoginLayout',
+    routes: [
+      { path: '/user', redirect: '/user/login' },
+      { path: '/user/login', component: './Login' },
+    ],
+  },
+  {
+    path: '/',
+    component: '../layouts/AuthLayout',
+    routes: [
+      { path: '/', redirect: '/dashboard' },
+      { path: '/dashboard', component: './Dashboard' },
+      {
+        path: '/moduleName',
+        name: 'moduleName',
+        routes: [{ path: '/moduleName/demo', component: './Demo' }],
+      },
+    ],
+  },
+];
