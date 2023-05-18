463a953f6115ad8a9ff8d3ac5f69b5280416e9b0
hejuewei <hejw@bncloud.onaliyun.com>
2023-05-18 08:26:28 +0800

    test

--- config/router.config.js
+++ config/router.config.js
@@ -7,17 +7,17 @@ export default [
       { path: '/user/login', component: './Login' },
     ],
   },
-  // {
-  //   path: '/',
-  //   component: '../layouts/AuthLayout',
-  //   routes: [
-  //     { path: '/', redirect: '/dashboard' },
-  //     { path: '/dashboard', component: './Dashboard' },
-  //     {
-  //       path: '/moduleName',
-  //       name: 'moduleName',
-  //       routes: [{ path: '/moduleName/demo', component: './Demo' }],
-  //     },
-  //   ],
-  // },
+  {
+    path: '/',
+    component: '../layouts/AuthLayout',
+    routes: [
+      { path: '/', redirect: '/dashboard' },
+      { path: '/dashboard', component: './Dashboard' },
+      {
+        path: '/moduleName',
+        name: 'moduleName',
+        routes: [{ path: '/moduleName/demo', component: './Demo' }],
+      },
+    ],
+  },
 ];
